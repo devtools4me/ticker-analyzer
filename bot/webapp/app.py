c70be2cdb5c1389c4837730f2c10f8e1ad22110a
sergey-astapov <serg.astapov@gmail.com>
2021-07-01 00:20:21 +0800

    - changes

---
+++ bot/webapp/app.py
@@ -0,0 +1,57 @@
+import re
+from flask import Flask, request
+import telegram
+from credentials import bot_token, bot_user_name, URL
+import yfinance as yf
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+
+    if text == "/start":
+        bot_welcome = """
+        Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+        """
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+    else:
+        try:
+            # clear the message we got from any non alphabets
+            text = re.sub(r"\W", "_", text)
+            tic = yf.Ticker(text.strip())
+            bot.sendMessage(chat_id=chat_id, text=str(tic.info), reply_to_message_id=msg_id)
+        except Exception:
+            # if things went wrong
+            bot.sendMessage(chat_id=chat_id,
+                            text="There was a problem in the name you used, please enter different name",
+                            reply_to_message_id=msg_id)
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+    return '.'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
---
+++ bot/webapp/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "1841422646:AAFCsCISuUn-BSKYWe2wHzpGi66yeMXKszk"
+bot_user_name = "TickerAnalyzerBot"
+URL = "the heroku app link that we will create later"
