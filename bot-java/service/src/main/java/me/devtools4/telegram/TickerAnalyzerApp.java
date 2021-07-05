e62388c4bc4b68dd726f6295a0e477448b801c9c
sergey-astapov <serg.astapov@gmail.com>
2021-07-06 00:09:35 +0800

    - changes

---
+++ bot-java/service/src/main/java/me/devtools4/telegram/TickerAnalyzerApp.java
@@ -0,0 +1,12 @@
+package me.devtools4.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerAnalyzerApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TickerAnalyzerApp.class, args);
+  }
+}
