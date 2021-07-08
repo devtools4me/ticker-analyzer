1419a1576e99cdaac1b64db74b6eb50486cc3a7b
sergey-astapov <serg.astapov@gmail.com>
2021-07-09 00:04:15 +0800

    - heroku run

--- bot-java/service/entrypoint.sh
+++ bot-java/service/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-/usr/bin/java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xmx256m -Xss512k -XX:MetaspaceSize=100m -jar /apps/app.jar
+java -XX:+UseG1GC -Xms256m -Xmx512m -jar /apps/app.jar
