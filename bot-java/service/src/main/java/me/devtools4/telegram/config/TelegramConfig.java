146cb83aa98cec0024103b22ed6671207b9f406d
sergey-astapov <serg.astapov@gmail.com>
2021-07-13 16:28:11 +0800

    - changes

--- bot-java/service/src/main/java/me/devtools4/telegram/config/TelegramConfig.java
+++ bot-java/service/src/main/java/me/devtools4/telegram/config/TelegramConfig.java
@@ -7,21 +7,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Profile("!webhook")
 public class TelegramConfig {
+
   @Value("${bot.userName}")
   private String userName;
 
   @Value("${bot.token}")
   private String token;
-
-  @Bean
-  public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
-    return new TelegramBotsApi(DefaultBotSession.class);
-  }
 
   @Bean
   public TickerBot tickerBot(CommandHandler commandHandler) {
