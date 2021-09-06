81adb826925b2949bcd4435e58392bd793ec82cd
sergey-astapov <serg.astapov@gmail.com>
2021-09-06 23:23:50 +0800

    - changes

--- bot-scala/src/main/scala/me/devtools4/ticker/telegram/package.scala
+++ bot-scala/src/main/scala/me/devtools4/ticker/telegram/package.scala
@@ -1,7 +1,7 @@
 package me.devtools4.ticker
 
-import me.devtools4.ticker.api.{TickerCmd, TickerQuery, UnknownCmd, UnknownQuery}
 import com.bot4s.telegram.models.Update
+import me.devtools4.ticker.api.{TickerCmd, TickerQuery, UnknownCmd, UnknownQuery}
 
 package object telegram {
 
@@ -15,17 +15,19 @@ package object telegram {
 
   object TelegramUpdate {
     def apply(update: Update): TelegramUpdate = update match {
-      case u if (u.message.exists(x => x.text.isDefined)) => TickerCmd(u.message.get.text.get) match {
-        case UnknownCmd(s) => UnknownUpdate(s)
-        case x => TextUpdate(u.message.get.chat.id, x)
-      }
-      case u if (u.callbackQuery.isDefined) => TickerQuery(u.callbackQuery.get.message.get.text.get) match {
-        case UnknownQuery(s) => UnknownUpdate(s)
-        case query => QueryCallback(
-          u.callbackQuery.get.message.get.chat.id,
-          u.callbackQuery.get.message.get.messageId,
-          query)
-      }
+      case Update(_, Some(m), _, _, _, _, _, None, _, _, _, _, _) if m.text.isDefined =>
+        TickerCmd(m.text.get) match {
+          case UnknownCmd(s) => UnknownUpdate(s)
+          case x => TextUpdate(m.chat.id, x)
+        }
+      case Update(_, None, _, _, _, _, _, Some(cb), _, _, _, _, _) if cb.message.exists(x => x.text.isDefined) =>
+        TickerQuery(cb.message.get.text.get) match {
+          case UnknownQuery(s) => UnknownUpdate(s)
+          case query => QueryCallback(
+            cb.message.get.chat.id,
+            cb.message.get.messageId,
+            query)
+        }
       case _ => UnknownUpdate(update.toString)
     }
   }
