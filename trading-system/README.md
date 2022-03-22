d737f34978ae100da790035040d7d1afd391f96c
sergey-astapov <serg.astapov@gmail.com>
2022-03-22 22:29:29 +0800

    - changes

---
+++ trading-system/README.md
@@ -0,0 +1,40 @@
+## PostgreSQL
+
+```
+docker run -d --name postgres13 \
+    -v postgres_data:/var/lib/postgresql/data \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_PASSWORD=postgres \
+    -p 5432:5432 \
+    postgres:latest
+```
+
+## pgAdmin
+
+```
+docker run -d --name pgAdmin \
+    -v pgadmin_data:/var/lib/pgadmin \
+    -e 'PGADMIN_DEFAULT_EMAIL=user@user.com' \
+    -e 'PGADMIN_DEFAULT_PASSWORD=admin' \
+    --add-host=host.docker.internal:host-gateway \
+    -p 54321:80 \
+    dpage/pgadmin4
+```
+
+### Open pgAdmin
+
+```
+127.0.0.1:54321
+user: user@user.com
+password: admin
+```
+
+### Add database
+
+```
+host: host.docker.internal
+port: 5432
+database: postgres
+user: postgres
+password: postgres
+```
---
+++ trading-system/orderbook-cmd/src/test/scala/me/devtools4/ts/orderbook/repository/PostgresRun.scala
@@ -0,0 +1,52 @@
+package me.devtools4.ts.orderbook.repository
+
+import me.devtools4.ts.config.DbConfig
+import me.devtools4.ts.dto.{OrderBookStartedEvent, Version}
+import me.devtools4.ts.orderbook.domain.OrderBookAggregate
+import me.devtools4.ts.orderbook.model.OrderBookEventEntity
+import org.flywaydb.core.Flyway
+import pureconfig._
+import pureconfig.generic.auto._
+import scalikejdbc.{ConnectionPool, GlobalSettings, LoggingSQLAndTimeSettings}
+
+import java.time.ZonedDateTime
+
+object PostgresRun extends App {
+
+  val config = ConfigSource.resources("db.conf").load[DbConfig] match {
+    case Right(conf) => conf
+    case Left(error) => throw new Exception(error.toString())
+  }
+
+  Class.forName(config.driver)
+  ConnectionPool.singleton(config.url, config.user, config.password)
+  GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
+    enabled = true,
+    singleLineMode = true,
+    logLevel = Symbol(config.logLevel).name
+  )
+
+  val ds = ConnectionPool.get().dataSource
+  val fw = Flyway.configure().dataSource(ds).load()
+  fw.migrate()
+
+  val repo = OrderBookEventStoreRepository()
+  val list = repo.findAll()
+  System.out.println(s"list = $list")
+
+  val event = OrderBookStartedEvent("", "", Version().nextVersion)
+  val saved = repo.save(OrderBookEventEntity(
+    ZonedDateTime.now(),
+    "TEST",
+    OrderBookAggregate.getClass.getTypeName,
+    Version().nextVersion.value,
+    event.getClass.getTypeName,
+    event))
+  System.out.println(s"saved = $saved")
+
+  val found = repo.findByAggregateId("TEST")
+  System.out.println(s"found = $found")
+
+  val all = repo.findAll()
+  System.out.println(s"all = $all")
+}
