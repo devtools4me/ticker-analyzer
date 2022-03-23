1bfea7a675bf5aa06c09e8ae0fbca3315fed8979
sergey-astapov <serg.astapov@gmail.com>
2022-03-23 23:28:16 +0800

    - changes

--- trading-system/orderbook-cmd/src/test/scala/me/devtools4/ts/orderbook/repository/OrderBookEventStoreRepositorySpec.scala
+++ trading-system/orderbook-cmd/src/test/scala/me/devtools4/ts/orderbook/repository/OrderBookEventStoreRepositorySpec.scala
@@ -1,12 +1,33 @@
 package me.devtools4.ts.orderbook.repository
 
+import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
 import org.junit.runner.RunWith
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.junit.JUnitRunner
+import scalikejdbc.{ConnectionPool, GlobalSettings, LoggingSQLAndTimeSettings}
 
 @RunWith(classOf[JUnitRunner])
-class OrderBookEventStoreRepositorySpec extends AnyFunSuite {
-  test("test") {
-    assertResult(true)(true)
+class OrderBookEventStoreRepositorySpec extends AnyFlatSpec with ForAllTestContainer {
+  override def container: PostgreSQLContainer = PostgreSQLContainer()
+  container.container.start()
+  Thread.sleep(10000)
+
+  it should "do something" in {
+    val driver = container.driverClassName
+    val url = container.jdbcUrl
+    val user = container.username
+    val password = container.password
+
+    Class.forName(driver)
+    ConnectionPool.singleton(url, user, password)
+    GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
+      enabled = true,
+      singleLineMode = true,
+      logLevel = Symbol("debug").name
+    )
+
+    val repo = OrderBookEventStoreRepository()
+
+    assertResult(repo.findAll())(List())
   }
 }
