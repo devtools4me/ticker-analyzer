cf1039c359acbdef9b31c55f395042ebb2af367c
sergey-astapov <serg.astapov@gmail.com>
2022-03-24 00:11:04 +0800

    - changes

--- trading-system/orderbook-cmd/src/test/scala/me/devtools4/ts/orderbook/repository/OrderBookEventStoreRepositorySpec.scala
+++ trading-system/orderbook-cmd/src/test/scala/me/devtools4/ts/orderbook/repository/OrderBookEventStoreRepositorySpec.scala
@@ -1,18 +1,31 @@
 package me.devtools4.ts.orderbook.repository
 
 import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import me.devtools4.ts.dto.{OrderBookStartedEvent, Version}
+import me.devtools4.ts.orderbook.domain.OrderBookAggregate
+import me.devtools4.ts.orderbook.model.OrderBookEventEntity
+import me.devtools4.ts.orderbook.repository.PostgresRun.repo
+import org.flywaydb.core.Flyway
 import org.junit.runner.RunWith
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.junit.JUnitRunner
 import scalikejdbc.{ConnectionPool, GlobalSettings, LoggingSQLAndTimeSettings}
 
-@RunWith(classOf[JUnitRunner])
-class OrderBookEventStoreRepositorySpec extends AnyFlatSpec with ForAllTestContainer {
-  override def container: PostgreSQLContainer = PostgreSQLContainer()
-  container.container.start()
-  Thread.sleep(10000)
+import java.time.ZonedDateTime
 
-  it should "do something" in {
+@RunWith(classOf[JUnitRunner])
+class OrderBookEventStoreRepositorySpec extends AnyFlatSpec
+  with BeforeAndAfterAll
+  with ForAllTestContainer
+{
+  override val container: PostgreSQLContainer = PostgreSQLContainer(
+    dockerImageNameOverride = "postgres:14.2"
+  )
+
+  private var sut: OrderBookEventStoreRepository = _
+
+  override def beforeAll(): Unit = {
     val driver = container.driverClassName
     val url = container.jdbcUrl
     val user = container.username
@@ -26,8 +39,30 @@ class OrderBookEventStoreRepositorySpec extends AnyFlatSpec with ForAllTestConta
       logLevel = Symbol("debug").name
     )
 
-    val repo = OrderBookEventStoreRepository()
+    val ds = ConnectionPool.get().dataSource
+    val fw = Flyway.configure().dataSource(ds).load()
+    fw.migrate()
 
-    assertResult(repo.findAll())(List())
+    sut = OrderBookEventStoreRepository()
+  }
+
+  it should "do something" in {
+    assertResult(sut.findAll())(List())
+
+    val event = OrderBookStartedEvent("", "", Version().nextVersion)
+    val saved = sut.save(OrderBookEventEntity(
+      ZonedDateTime.now(),
+      "TEST",
+      OrderBookAggregate.getClass.getTypeName,
+      Version().nextVersion.value,
+      event.getClass.getTypeName,
+      event))
+    assertResult(saved.isDefined)(true)
+
+    val found = sut.findByAggregateId("TEST")
+    assertResult(found.isEmpty)(false)
+
+    val all = sut.findAll()
+    assertResult(found.isEmpty)(false)
   }
 }
